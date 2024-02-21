e9a552d06b35fc2c31e0a246df197e8302a1467a
AllanC12 <allandevfront@gmail.com>
2024-02-20 23:02:46 -0300

    criar interfaces para musica , album e track. Refatorar titulos nas box de music

--- interfaces/dataMusic.ts
+++ interfaces/dataMusic.ts
@@ -1,7 +1,40 @@
-export interface IMusicChart {
-  album: Object,
-  artist: Object,
-  playlist: Object,
-  podcast: Object,
-  track: Object
+export interface ITrack {
+  id: number,
+  title: string,
+  artist: {
+    name: string
+  },
+  preview: string
 }
+
+export interface IArtist {
+  id: number,
+  name: string,
+  link: string
+}
+
+export interface IAlbum {
+  id: number,
+  artist: {
+    name:string,
+    picture_big:string
+  }
+}
+
+export interface IMusic {
+  title:string,
+  name: string,
+  album: Object,
+  artist: {
+    name:string,
+    picture_big:string
+  },
+  picture_big: string
+  preview: string
+}
+
+export interface IData {
+  data: [],
+  total: number
+}
+
--- pages/ts_js/home/home.ts
+++ pages/ts_js/home/home.ts
@@ -1,19 +1,22 @@
-import { IMusicChart } from "../../../interfaces/dataMusic"
+import { ITrack,IArtist,IAlbum,IMusic,IData } from "../../../interfaces/dataMusic";
 
 let linkVisited: HTMLElement;
-const linksNav = document.getElementById('nav-links')?.children as HTMLCollection
-const linkTracks = document.getElementById('tracks') as HTMLElement
-const linkArtists = document.getElementById('artists') as HTMLElement
+const linksNav = document.getElementById("nav-links")
+  ?.children as HTMLCollection;
+const linkTracks = document.getElementById("tracks") as HTMLElement;
+const linkArtists = document.getElementById("artists") as HTMLElement;
 const linkAlbums = document.getElementById("albums") as HTMLElement;
-const contentLibrary = document.querySelector(".content-library") as HTMLElement;
-const player = document.getElementById('player') as HTMLElement
+const contentLibrary = document.querySelector(
+  ".content-library"
+) as HTMLElement;
+const player = document.getElementById("player") as HTMLElement;
 
-const styleLinks = (link:HTMLElement,linkVisited:HTMLElement):void => {
+const styleLinks = (link: HTMLElement, linkVisited: HTMLElement): void => {
   link?.style.setProperty("background-color", "#262525");
-  link?.style.setProperty("color", "#fff")
-  linkVisited?.style.setProperty('background-color','#faf5f7')
-  linkVisited?.style.setProperty('color','#000')
-}
+  link?.style.setProperty("color", "#fff");
+  linkVisited?.style.setProperty("background-color", "#faf5f7");
+  linkVisited?.style.setProperty("color", "#000");
+};
 
 const playMusic = (source: string | null) => {
   if (source) {
@@ -22,59 +25,63 @@ const playMusic = (source: string | null) => {
   }
 };
 
-const verifyMusiList = () => {
+const verifyMusicList = () => {
   setTimeout(() => {
     if (contentLibrary.children.length > 0) {
       for (let i = 0; i < contentLibrary.children.length; i++) {
         contentLibrary.children[i].addEventListener("click", () => {
           let linkPreview: string | null =
-            contentLibrary.children[i].children[0].getAttribute("preview");
-          console.log(player.getAttribute("src"));
+          contentLibrary.children[i].children[0].getAttribute("preview");
           playMusic(linkPreview);
         });
       }
     }
   }, 2000);
-
-}
+};
 
 for (let i = 0; i < linksNav.length; i++) {
-  linksNav[i].addEventListener("click", (e) => {   
+  linksNav[i].addEventListener("click", (e) => {
     e.preventDefault();
     switch (linksNav[i].id) {
       case "tracks":
-        contentLibrary.innerHTML = ``
+        contentLibrary.innerHTML = ``;
         insertData("https://api.deezer.com/chart/0/tracks");
-        styleLinks(linkTracks,linkVisited);
-        verifyMusiList()
-        linkVisited = linkTracks
+        styleLinks(linkTracks, linkVisited);
+        verifyMusicList();
+        linkVisited = linkTracks;
         break;
       case "artists":
-        contentLibrary.innerHTML = ``
+        contentLibrary.innerHTML = ``;
         insertData("https://api.deezer.com/chart/0/artists");
-        styleLinks(linkArtists,linkVisited);
-        verifyMusiList()
-        linkVisited = linkArtists
+        styleLinks(linkArtists, linkVisited);
+        verifyMusicList();
+        linkVisited = linkArtists;
         break;
       case "albums":
-        contentLibrary.innerHTML = ``
+        contentLibrary.innerHTML = ``;
         insertData("https://api.deezer.com/chart/0/albums");
-        styleLinks(linkAlbums,linkVisited);
-        verifyMusiList()
-        linkVisited = linkAlbums
+        styleLinks(linkAlbums, linkVisited);
+        verifyMusicList();
+        linkVisited = linkAlbums;
         break;
     }
   });
 }
 
-const connectApi = async (endPoint: string): Promise<IMusicChart> => {
+const connectApi = async (endPoint: string): Promise<IData> => {
   const resp = await fetch(endPoint).then((resp) => resp.json());
   return resp;
 };
 
-const constructLayout = (title: string, image: String,preview: string): void => {
-
+const constructLayout = (
+  title: string,
+  artist: string,
+  image: String,
+  preview: string
+): void => {
   const boxMusic = document.createElement("div");
+  const titleMusic: string = title === artist ? "" : title;
+  const artistMusic: string | undefined = artist ? artist : "";
 
   boxMusic.classList.add("box-music");
   boxMusic.innerHTML = `
@@ -84,7 +91,7 @@ const constructLayout = (title: string, image: String,preview: string): void => 
     </div>
      <div class="title-music">
        <p class="title">
-           ${title}
+           ${titleMusic} - ${artistMusic}
        </p>
        </div>
     </div>
@@ -93,27 +100,18 @@ const constructLayout = (title: string, image: String,preview: string): void => 
 };
 
 const insertData = (url: string): void => {
-  connectApi(url).then((resp: any) => {
-    resp.data.map((album: any) => {
-      const imageLayout: String =
-        url.includes("albums") || url.includes("tracks")
-          ? album.artist.picture_big
-          : album.picture_big;
-      const artist: String = url.includes("tracks")
-        ? album.artist.name
-        : album.name;
-      const title: String = url.includes("artists") ? album.name : album.title;
-      const preview: string = url.includes("tracks") ? album.preview : "";
-      constructLayout(`${title} - ${artist}`, imageLayout, preview);
+  connectApi(url).then((resp: IData) => {
+    console.log(resp);
+    resp.data.map((cover:IMusic) => {
+      const imageLayout: string = url.includes("albums") || url.includes("tracks") ? cover.artist.picture_big : cover.picture_big;
+      const artist: string = url.includes("tracks") ? cover.artist.name : cover.name;
+      const title: string = url.includes("artists") ? cover.name : cover.title;
+      const preview: string = url.includes("tracks") ? cover.preview : "";
+      constructLayout(title, artist, imageLayout, preview);
     });
   });
 };
 
 insertData("https://api.deezer.com/chart/0/tracks");
-
-
-verifyMusiList()
-
-
-
+verifyMusicList();
 
